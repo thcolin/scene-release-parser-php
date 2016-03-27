83161914eccf8db64dd32a3509a5535c33c29323
thcolin <th.colin3@gmail.com>
2016-03-28 01:51:24 +0200

    Clean (const, setters, logic)
    No impact on unit tests

--- src/Release.php
+++ src/Release.php
@@ -2,6 +2,7 @@
 
 	namespace thcolin\SceneReleaseParser;
 
+	use InvalidArgumentException;
 	use Exception;
 
 	class Release{
@@ -12,17 +13,51 @@
 		const ORIGINAL_RELEASE = 1;
 		const GENERATED_RELEASE = 2;
 
-		protected static $sourceStatic = [
-			'DVDRip' => [
+		const SOURCE = 'source';
+		const SOURCE_DVDRIP = 'DVDRip';
+		const SOURCE_DVDSCR = 'DVDScr';
+		const SOURCE_WEB_DL = 'WEB-DL';
+		const SOURCE_BRRIP = 'BRRip';
+		const SOURCE_BDRIP = 'BDRip';
+		const SOURCE_DVD_R = 'DVD-R';
+		const SOURCE_R5 = 'R5';
+		const SOURCE_HDRIP = 'HDRip';
+		const SOURCE_BLURAY = 'BLURAY';
+		const SOURCE_PDTV = 'PDTV';
+		const SOURCE_SDTV = 'SDTV';
+
+		const ENCODING = 'encoding';
+		const ENCODING_XVID = 'XviD';
+		const ENCODING_DIVX = 'DivX';
+		const ENCODING_X264 = 'x264';
+		const ENCODING_X265 = 'x265';
+		const ENCODING_H264 = 'h264';
+
+		const RESOLUTION = 'resolution';
+		const RESOLUTION_SD = 'SD';
+		const RESOLUTION_720P = '720p';
+		const RESOLUTION_1080P = '1080p';
+
+		const DUB = 'dub';
+		const DUB_DUBBED = 'DUBBED';
+		const DUB_AC3 = 'AC3';
+		const DUB_MD = 'MD';
+		const DUB_LD = 'LD';
+
+		const LANGUAGE_MULTI = 'MULTI';
+		const LANGUAGE_DEFAULT = 'VO';
+
+		public static $sourceStatic = [
+			self::SOURCE_DVDRIP => [
 				'dvdrip',
 				'dvd-rip'
 			],
-			'DVDScr' => [
+			self::SOURCE_DVDSCR => [
 				'dvdscr',
 				'dvd-scr',
 				'dvdscreener'
 			],
-			'WEB-DL' => [
+			self::SOURCE_WEB_DL => [
 				'webtv',
 				'web-tv',
 				'webdl',
@@ -31,22 +66,22 @@
 				'webhd',
 				'web'
 			],
-			'BRRip' => [
+			self::SOURCE_BRRIP => [
 				'brrip',
 				'br-rip'
 			],
-			'BDRip' => [
+			self::SOURCE_BDRIP => [
 				'bdrip',
 				'bd-rip'
 			],
-			'DVD-R' => [
+			self::SOURCE_DVD_R => [
 				'dvd',
 				'dvd-r'
 			],
-			'R5' => [
+			self::SOURCE_R5 => [
 				'r5'
 			],
-			'HDRip' => [
+			self::SOURCE_HDRIP => [
 				'hdtv',
 				'hdtvrip',
 				'hdtv-rip',
@@ -55,68 +90,72 @@
 				'mhd',
 				'hd'
 			],
-			'BLURAY' => [
+			self::SOURCE_BLURAY => [
 				'bluray',
 				'blu-ray'
 			],
-			'PDTV' => [
+			self::SOURCE_PDTV => [
 				'pdtv'
 			],
-			'SDTV' => [
+			self::SOURCE_SDTV => [
 				'sdtv'
 			]
 		];
 
-		protected static $encodingStatic = [
-			'DivX' => [
+		public static $encodingStatic = [
+			self::ENCODING_DIVX => [
 				'divx'
 			],
-			'XviD' => [
+			self::ENCODING_XVID => [
 				'xvid'
 			],
-			'x264' => [
+			self::ENCODING_X264 => [
 				'x264',
 				'x.264'
 			],
-			'h264' => [
+			self::ENCODING_X265 => [
+				'x265',
+				'x.265'
+			],
+			self::ENCODING_H264 => [
 				'h264',
 				'h.264'
 			]
 		];
 
-		protected static $resolutionStatic = [
-			'SD' => [
+		public static $resolutionStatic = [
+			self::RESOLUTION_SD => [
 				'sd'
 			],
-			'720p' => [
+			self::RESOLUTION_720P => [
 				'720p'
 			],
-			'1080p' => [
+			self::RESOLUTION_1080P => [
 				'1080p'
 			]
 		];
 
-		protected static $dubStatic = [
-			'DUBBED' => [
+		public static $dubStatic = [
+			self::DUB_DUBBED => [
 				'dubbed'
 			],
-			'AC3' => [
+			self::DUB_AC3 => [
 				'ac3.dubbed',
 				'ac3'
 			],
-			'MD' => [
+			self::DUB_MD => [
 				'md',
 				'microdubbed',
 				'micro-dubbed'
 			],
-			'LD' => [
+			self::DUB_LD => [
 				'ld',
 				'linedubbed',
 				'line-dubbed'
 			]
 		];
 
-		protected static $languageStatic = [
+		public static $languageStatic = [
 			'FRENCH' => [
 				'FRENCH',
 				'Français',
@@ -180,10 +219,10 @@
 			'UKRAINIAN' => 'UKRAINIAN',
 			'VIETNAMESE' => 'VIETNAMESE',
 			'WELSH' => 'WELSH',
-			'MULTI' => 'MULTI'
+		 	self::LANGUAGE_MULTI => 'MULTI'
 		];
 
-		protected static $flagsStatic = [
+		public static $flagsStatic = [
 			'PROPER' => 'PROPER',
 			'FASTSUB' => 'FASTSUB',
 			'LIMITED' => 'LIMITED',
@@ -244,154 +283,68 @@
 			]
 		];
 
-		protected $release;
-		protected $type;
-		protected $title;
-		protected $year;
-		protected $language;
-		protected $resolution;
-		protected $source;
-		protected $dub;
-		protected $encoding;
-		protected $group;
-		protected $flags;
+		protected $release = null;
+		protected $type = null;
+		protected $title = null;
+		protected $year = null;
+		protected $language = null;
+		protected $resolution = null;
+		protected $source = null;
+		protected $dub = null;
+		protected $encoding = null;
+		protected $group = null;
+		protected $flags = null;
 
-		protected $season;
-		protected $episode;
+		protected $season = null;
+		protected $episode = null;
 
-		public function __construct($release){
-			$this -> original = $release;
+		public function __construct($name){
+			// CLEAN
+			$cleaned = $this -> clean($name);
 
-			// Clean
-			$this -> release = str_replace(['[', ']', '(', ')', ',', ';', ':', '!'], '.', $this -> original);
-			$this -> release = preg_replace('#[\s]+#', ' ', $this -> release);
-			$this -> release = str_replace(' ', '.', $this -> release);
-			$this -> title = $this -> release;
-
-			// Positions
-			$this -> positions = explode('.', $this -> release);
+			$this -> original = $name;
+			$this -> release = $cleaned;
+			$title = $cleaned;
 
 			// LANGUAGE
-			$languages = [];
+			$language = $this -> parseLanguage($title);
+			$this -> setLanguage($language);
 
-			foreach(Release::$languageStatic as $langue => $patterns){
-				if(!is_array($patterns)){
-					$patterns = [$patterns];
-				}
+			// SOURCE
+			$source = $this -> parseSource($title);
+			$this -> setSource($source);
 
-				foreach($patterns as $pattern){
-					$this -> title = preg_replace('#[\.|\-]'.preg_quote($pattern).'([\.|\-| ])#i', '$1', $this -> title, 1, $replacements);
-					if($replacements > 0){
-						$languages[] = $langue;
-						break;
-					}
-				}
-			}
+			// ENCODING
+			$encoding = $this -> parseEncoding($title);
+			$this -> setEncoding($encoding);
 
-			if(count($languages) == 1){
-				$this -> language = $languages[0];
-			} else if(count($languages) > 1){
-				$this -> language = 'MULTI';
-			}
+			// RESOLUTION
+			$resolution = $this -> parseResolution($title);
+			$this -> setResolution($resolution);
 
-			// SOURCE, ENCODING, RESOLUTION, DUB, LANGUAGE (unique)
-			foreach(['source', 'encoding', 'resolution', 'dub'] as $attribute){
-				$attributes = $attribute.'Static';
-
-				foreach(Release::$$attributes as $key => $patterns){
-					if(!is_array($patterns)){
-						$patterns = [$patterns];
-					}
-
-					foreach($patterns as $pattern){
-						$this -> title = preg_replace('#[\.|\-]'.preg_quote($pattern).'([\.|\-])?#i', '$1', $this -> title, 1, $replacements);
-						if($replacements > 0){
-							$this -> $attribute = $key;
-						}
-					}
-
-					if(isset($this -> $attribute)){
-						break;
-					}
-				}
-			}
+			// DUB
+			$dub = $this -> parseDub($title);
+			$this -> setDub($dub);
 
 			// YEAR
-			$this -> title = preg_replace_callback('#[\.|\-](\d{4})([\.|\-])?#', function($matches){
-				$this -> year = $matches[1];
-				return (isset($matches[2]) ? $matches[2]:'');
-			}, $this -> title, 1);
+			$year = $this -> parseYear($title);
+			$this -> setYear($year);
 
-			// FLAGS (multiple)
-			foreach(Release::$flagsStatic as $key => $patterns){
-				if(!is_array($patterns)){
-					$patterns = [$patterns];
-				}
-
-				foreach($patterns as $pattern){
-					$this -> title = preg_replace('#[\.|\-]'.preg_quote($pattern).'([\.|\-])?#i', '$1', $this -> title, 1, $replacements);
-					if($replacements > 0){
-						$this -> flags[] = $key;
-					}
-				}
-			}
+			// FLAGS
+			$flags = $this -> parseFlags($title);
+			$this -> setFlags($flags);
 
 			// TYPE
-			$this -> title = preg_replace_callback('#[\.\-]S(\d+)[\.\-]?(E(\d+))?([\.\-])#i', function($matches){
-				$this -> type = self::TVSHOW;
-				// 01 -> 1 (numeric)
-				$this -> season = intval($matches[1]);
-
-				if($matches[3]){
-					$this -> episode = intval($matches[3]);
-				}
-				return $matches[4];
-			}, $this -> title, 1, $count);
-
-			if($count == 0){
-
-				// Not a Release
-				if(
-					!$this -> resolution &&
-					!$this -> source &&
-					!$this -> dub &&
-					!$this -> encoding
-				){
-					throw new Exception('The string "'.$this -> release.'" is not a correct Scene Release name');
-				}
-
-				// movie
-				$this -> type = self::MOVIE;
-			}
+			$type = $this -> parseType($title);
+			$this -> setType($type);
 
 			// GROUP
-			$this -> title = preg_replace_callback('#\-([a-zA-Z0-9_\.]+)$#', function($matches){
-				if(strlen($matches[1]) > 12){
-					preg_match('#(\w+)#', $matches[1], $matches);
-				}
-				$this -> group = preg_replace('#^\.+|\.+$#', '', $matches[1]);
-				return '';
-			}, $this -> title);
-
-			// Create and clean title by positions
-			$title = [];
-			$this -> title = preg_replace('#\.+#', '.', $this -> title);
-			foreach(array_intersect($this -> positions, explode('.', $this -> title)) as $key => $value){
-				$last = isset($last) ? $last:0;
-
-				if($key - $last > 1){
-					$this -> title = implode(' ', $title);
-					break;
-				}
-
-				$title[] = $value;
-				$this -> title = implode(' ', $title);
-				$last = $key;
-			}
+			$group = $this -> parseGroup($title);
+			$this -> setGroup($group);
 
 			// TITLE
-			$this -> title = ucwords(strtolower($this -> title));
-			$this -> title = trim($this -> title);
+			$title = $this -> parseTitle($title);
+			$this -> setTitle($title);
 		}
 
 		public function __toString(){
@@ -428,80 +381,305 @@
 			return $this -> release;
 		}
 
+		private function clean($name){
+			$release = str_replace(['[', ']', '(', ')', ',', ';', ':', '!'], '.', $name);
+			$release = preg_replace('#[\s]+#', ' ', $release);
+			$release = str_replace(' ', '.', $release);
+
+			return $release;
+		}
+
+		public function guess(){
+			if(!isset($this -> year)){
+				$this -> setYear($this -> guessYear());
+			}
+
+			if(!isset($this -> resolution)){
+				$this -> setResolution($this -> guessResolution());
+			}
+
+			if(!isset($this -> language)){
+				$this -> setLanguage($this -> guessLanguage());
+			}
+
+			return $this;
+		}
+
+		private function parseAttribute(&$title, $attribute){
+			if(!in_array($attribute, [self::SOURCE, self::ENCODING, self::RESOLUTION, self::DUB])){
+				throw new InvalidArgumentException();
+			}
+
+			$attributes = $attribute.'Static';
+
+			foreach(Release::$$attributes as $key => $patterns){
+				if(!is_array($patterns)){
+					$patterns = [$patterns];
+				}
+
+				foreach($patterns as $pattern){
+					$title = preg_replace('#[\.|\-]'.preg_quote($pattern).'([\.|\-])?#i', '$1', $title, 1, $replacements);
+					if($replacements > 0){
+						return $key;
+					}
+				}
+			}
+
+			return null;
+		}
+
 		public function getType(){
 			return $this -> type;
+		}
+
+		private function parseType(&$title){
+			$type = null;
+
+			$title = preg_replace_callback('#[\.\-]S(\d+)[\.\-]?(E(\d+))?([\.\-])#i', function($matches) use (&$type){
+				$type = self::TVSHOW;
+				// 01 -> 1 (numeric)
+				$this -> setSeason(intval($matches[1]));
+
+				if($matches[3]){
+					$this -> setEpisode(intval($matches[3]));
+				}
+				return $matches[4];
+			}, $title, 1, $count);
+
+			if($count == 0){
+				// Not a Release
+				if(
+					!isset($this -> resolution) &&
+					!isset($this -> source) &&
+					!isset($this -> dub) &&
+					!isset($this -> encoding)
+				){
+					throw new Exception('This is not a correct Scene Release name');
+				}
+
+				// movie
+				$type = self::MOVIE;
+			}
+
+			return $type;
+		}
+
+		public function setType($type){
+			$this -> type = $type;
 		}
 
 		public function getTitle(){
 			return $this -> title;
 		}
 
+		private function parseTitle(&$title){
+			$array = [];
+			$title = preg_replace('#\.+#', '.', $title);
+			$positions = explode('.', $this -> release);
+
+			foreach(array_intersect($positions, explode('.', $title)) as $key => $value){
+				$last = isset($last) ? $last:0;
+
+				if($key - $last > 1){
+					$return = implode(' ', $array);
+					break;
+				}
+
+				$array[] = $value;
+				$return = implode(' ', $array);
+				$last = $key;
+			}
+
+			$return = ucwords(strtolower($return));
+			$return = trim($return);
+
+			return $return;
+		}
+
+		public function setTitle($title){
+			$this -> title = $title;
+		}
+
 		public function getSeason(){
 			return $this -> season;
+		}
+
+		public function setSeason($season){
+			$this -> season = $season;
 		}
 
 		public function getEpisode(){
 			return $this -> episode;
 		}
 
+		public function setEpisode($episode){
+			$this -> episode = $episode;
+		}
+
 		public function getLanguage(){
 			return $this -> language;
 		}
 
+		private function parseLanguage(&$title){
+			$languages = [];
+
+			foreach(Release::$languageStatic as $langue => $patterns){
+				if(!is_array($patterns)){
+					$patterns = [$patterns];
+				}
+
+				foreach($patterns as $pattern){
+					$title = preg_replace('#[\.|\-]'.preg_quote($pattern).'([\.|\-| ])#i', '$1', $title, 1, $replacements);
+					if($replacements > 0){
+						$languages[] = $langue;
+						break;
+					}
+				}
+			}
+
+			if(count($languages) == 1){
+				return $languages[0];
+			} else if(count($languages) > 1){
+				return self::LANGUAGE_MULTI;
+			} else{
+				return null;
+			}
+		}
+
 		public function guessLanguage(){
-			return 'VO';
+			return self::LANGUAGE_DEFAULT;
+		}
+
+		public function setLanguage($language){
+			$this -> language = $language;
 		}
 
 		public function getResolution(){
 			return $this -> resolution;
 		}
 
+		private function parseResolution(&$title){
+			return $this -> parseAttribute($title, self::RESOLUTION);
+		}
+
 		public function guessResolution(){
 			return 'SD';
+		}
+
+		public function setResolution($resolution){
+			$this -> resolution = $resolution;
 		}
 
 		public function getSource(){
 			return $this -> source;
 		}
 
+		private function parseSource(&$title){
+			return $this -> parseAttribute($title, self::SOURCE);
+		}
+
+		public function setSource($source){
+			$this -> source = $source;
+		}
+
 		public function getDub(){
 			return $this -> dub;
+		}
+
+		private function parseDub(&$title){
+			return $this -> parseAttribute($title, self::DUB);
+		}
+
+		public function setDub($dub){
+			$this -> dub = $dub;
 		}
 
 		public function getEncoding(){
 			return $this -> encoding;
 		}
 
+		private function parseEncoding(&$title){
+			return $this -> parseAttribute($title, self::ENCODING);
+		}
+
+		public function setEncoding($encoding){
+			$this -> encoding = $encoding;
+		}
+
 		public function getYear(){
 			return $this -> year;
+		}
+
+		private function parseYear(&$title){
+			$year = null;
+
+			$title = preg_replace_callback('#[\.|\-](\d{4})([\.|\-])?#', function($matches) use (&$year){
+				if(isset($matches[1])){
+					$year = $matches[1];
+				}
+
+				return (isset($matches[2]) ? $matches[2]:'');
+			}, $title, 1);
+
+			return $year;
 		}
 
 		public function guessYear(){
 			return date('Y');
 		}
 
+		public function setYear($year){
+			$this -> year = $year;
+		}
+
 		public function getGroup(){
 			return $this -> group;
+		}
+
+		private function parseGroup(&$title){
+			$group = null;
+
+			$title = preg_replace_callback('#\-([a-zA-Z0-9_\.]+)$#', function($matches) use (&$group){
+				if(strlen($matches[1]) > 12){
+					preg_match('#(\w+)#', $matches[1], $matches);
+				}
+
+				$group = preg_replace('#^\.+|\.+$#', '', $matches[1]);
+				return '';
+			}, $title);
+
+			return $group;
+		}
+
+		public function setGroup($group){
+			$this -> group = $group;
 		}
 
 		public function getFlags(){
 			return $this -> flags;
 		}
 
-		public function guess(){
-			if(!isset($this -> year)){
-				$this -> year = $this -> guessYear();
+		private function parseFlags(&$title){
+			$flags = [];
+
+			foreach(Release::$flagsStatic as $key => $patterns){
+				if(!is_array($patterns)){
+					$patterns = [$patterns];
+				}
+
+				foreach($patterns as $pattern){
+					$title = preg_replace('#[\.|\-]'.preg_quote($pattern).'([\.|\-])?#i', '$1', $title, 1, $replacements);
+					if($replacements > 0){
+						$flags[] = $key;
+					}
+				}
 			}
 
-			if(!isset($this -> resolution)){
-				$this -> resolution = $this -> guessResolution();
-			}
+			return $flags;
+		}
 
-			if(!isset($this -> language)){
-				$this -> language = $this -> guessLanguage();
-			}
-
-			return $this;
+		public function setFlags($flags){
+			$this -> flags = (is_array($flags) ? $flags:[$flags]);
 		}
 
 	}
